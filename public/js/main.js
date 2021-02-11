77f1890e848f99f33f139b1504bce55c7bce6228
gustavovilioni <60745816+gustavovilioni@users.noreply.github.com>
2021-02-11 13:21:22 -0300

    Aula 02

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,12 @@ var tamanhoFrase = $('#tamanho-frase')
 
 tamanhoFrase.text(numeroPalavras)
 
-console.log(numeroPalavras)
-console.log(frase)
-console.log(tamanhoFrase)
+var campo = $('.campo-digitacao')
+campo.on("input", ()=>{
+    var conteudo = campo.val()
+    var palavras = conteudo.split(/\S+/).length -1
+    var caracteres = conteudo.split("").length // OU conteudo.length
+
+    $('#contador-palavras').text(palavras)
+    $('#contador-caracteres').text(caracteres)
+})
